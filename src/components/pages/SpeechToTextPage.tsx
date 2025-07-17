1cd759dfff3e2de40223d4b5f87188e7dafd37b6
AraArash44 <arashasghari1245@gmail.com>
2025-07-18 02:36:20 +0330

    upload file section added

--- src/components/pages/SpeechToTextPage.tsx
+++ src/components/pages/SpeechToTextPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import MainLayout from "../layouts/MainLayout";
 import Tabs from "../common/Tabs";
 import TabsWithMenu from '../common/TabsWithMenu';
@@ -12,11 +12,13 @@ import AudioPlayer from '../common/AudioPlayer';
 import RowsTable from '../common/Rows';
 
 const SpeechToTextPage = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [ uploadedAudio, setuploadedAudio ] = useState<Blob | null>(null);
   const { recording, audio, micLevel, startRecording, stopRecording } = useAudioRecorder();
   const { loading, error, data, postData } = usePost<MediaInput, TranscriptionOutput>({
     url: "/transcribe_files/", headers: {Authorization: `Token ${import.meta.env.VITE_API_KEY}` },
   });
-  const { uploading, errorUploading, fileUrl } = useBytescaleUploader(audio);
+  const { uploading, errorUploading, fileUrl } = useBytescaleUploader(audio || uploadedAudio);
   const scale = useMemo(() => 1 + micLevel * 0.5, [micLevel]);
   const icon = useMemo(() => (recording ? '/images/icons/pause.svg' : '/images/icons/mic-white.svg'),[recording]);
   const handleToggleRecording = async () => {
@@ -24,6 +26,15 @@ const SpeechToTextPage = () => {
       stopRecording();
     } else {
       startRecording();
+    }
+  };
+  const handleUploadButtonClicked = () => {
+    inputRef.current?.click();
+  }
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setuploadedAudio(file);
     }
   };
 
@@ -87,7 +98,36 @@ const SpeechToTextPage = () => {
             </div>
           </Tabs.Tab>
           <Tabs.Tab title="بارگذاری فایل" icon="upload" theme="blue">
-            <div className="w-150 h-80"></div>
+            <div className="flex flex-col justify-center items-center w-150 h-80">
+              {!loading && !uploading ? (
+                !data ? (
+                  <>
+                    <input type="file" ref={inputRef} onChange={handleFileChange} className="hidden" />
+                    <button onClick={handleUploadButtonClicked} className="bg-blue rounded-full w-16 h-16 cursor-pointer transition-transform duration-30">
+                      <img src="/images/icons/upload-white.svg" alt="upload" className="m-auto w-8 h-8" />
+                    </button>
+                    <p className="text-neutral-200 font-light text-center mt-2">برای بارگذاری فایل گفتاری (صوتی/تصویری)، دکمه را فشار دهید<br/>متن پیاده شده آن، در اینجا ظاهر می شود</p>
+                  </>
+                ) : (
+                  <TabsWithMenu defaultIndex={0} hasDownload={true} hasCopy={true} hasTryAgain={true} theme='blue'>
+                    <TabsWithMenu.Tab title='متن ساده' icon='text'>
+                      <p className='font-light'>{data[0].segments.reduce((acc, cur) => acc.concat(cur["text"] + " "), "")}</p>
+                      <div className="absolute bottom-0 w-[94%] mb-5">
+                        <AudioPlayer src={fileUrl!} theme="blue"/>
+                      </div>
+                    </TabsWithMenu.Tab>
+                    <TabsWithMenu.Tab title='متن زمان‌بندی شده' icon='time'>
+                      <RowsTable texts={data[0].segments} />
+                      <div className="absolute bottom-0 w-[94%] mb-5 pt-2 bg-neutral-white">
+                        <AudioPlayer src={fileUrl!} theme="blue"/>
+                      </div>
+                    </TabsWithMenu.Tab>
+                  </TabsWithMenu>
+                )
+              ) : (
+                <span className="loader"></span>
+              )}
+            </div>
           </Tabs.Tab>
           <Tabs.Tab title="لینک" icon="chain" theme="red">
             <div className="w-150 h-80"></div>
